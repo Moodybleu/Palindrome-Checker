d51672f221c08e819b9917d53eb679378f6c973c
Moodybleu <meganbenn27@icloud.com>
2022-12-27 08:30:16 -0800

    Adding Palindrome function

--- script.js
+++ script.js
@@ -3,11 +3,22 @@ const result = document.querySelector(".result")
 
 BigInt.addEventlistener("click", palindrome)
 
-// madam
-//noon
+// madam - TEST WORD
+// noon - TEST WORD
 function palindrome() {
     const word = document.querySelector(".input-text").value;
     let len = word.length;
 
-    let start = word.substring(0, Math.floor(len / 2));
+    let start = word.substring(0, Math.floor(len / 2)).toLowerCase();
+    let end = word.substring(0, Math.floor(len / 2)).toLowerCase();
+
+    let flip = end.split("").reverse().join("");
+    if (start == flip) {
+        result.innerHTML = `${word.toUpperCase()} is a palindrome`
+    } else {
+        result.innerHTML = `${word.toUpperCase()} is NOT a palindrome`;
+    }
+    // let flip = [...end].reverse().join("");
+
+    // alert(flip);
 }
