02ca6375080e887773d4537741f912a94fc37203
Moodybleu <meganbenn27@icloud.com>
2022-12-27 14:16:14 -0800

    used completed code to fix bug/ Complete Project

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-const btn = document.querySelector(".btn")
-const result = document.querySelector(".result")
+const btn = document.querySelector(".btn");
+const result = document.querySelector(".result");
 
-BigInt.addEventlistener("click", palindrome)
+btn.addEventListener("click", palindrome);
 
 // madam - TEST WORD
 // noon - TEST WORD
 function palindrome() {
     const word = document.querySelector(".input-text").value;
     let len = word.length;
-
+  
     let start = word.substring(0, Math.floor(len / 2)).toLowerCase();
-    let end = word.substring(0, Math.floor(len / 2)).toLowerCase();
-
-    let flip = end.split("").reverse().join("");
+    let end = word.substring(len - Math.floor(len / 2)).toLowerCase();
+  
+    // let flip = end.split("").reverse().join("");
+  
+    let flip = [...end].reverse().join("");
     if (start == flip) {
-        result.innerHTML = `${word.toUpperCase()} is a palindrome`
+      result.innerHTML = `${word.toUpperCase()} is a palindrome`;
     } else {
-        result.innerHTML = `${word.toUpperCase()} is NOT a palindrome`;
+      result.innerHTML = `${word.toUpperCase()} is NOT a palindrome`;
     }
-    // let flip = [...end].reverse().join("");
-
     // alert(flip);
-}
+  }
